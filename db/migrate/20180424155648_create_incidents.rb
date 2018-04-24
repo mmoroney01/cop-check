b408eba54772b7fea614be9d0a25fccc4d0b913d
Michael Moroney <mmoroney06@gmail.com>
2018-04-24 12:17:43 -0500

    WIP geocoder logic

--- db/migrate/20180424155648_create_incidents.rb
+++ db/migrate/20180424155648_create_incidents.rb
@@ -1,7 +1,12 @@
 class CreateIncidents < ActiveRecord::Migration[5.1]
   def change
     create_table :incidents do |t|
-      t.string :location, null: false
+      t.string :street
+      t.string :city
+      t.string :zip
+      t.string :state
+      t.float :latitude
+      t.float :longitude
       t.string :description, null: false
       
       t.timestamps
