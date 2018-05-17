26dd7f3b51477bfccb23911672ccc92da965cf05
Michael Moroney <mmoroney06@gmail.com>
2018-05-17 13:47:16 -0500

    add button to file incident page, redirect to home if user inputs no address

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -9,15 +9,23 @@ class IndexController < ApplicationController
   def search
     address = params[:address].gsub(/\s/,'+')
 
-    url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{address}&key=#{ENV['GEOCODER']}"
+    if address.empty?
+      redirect_to :action => "index"
+    else
+      url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{address}&key=#{ENV['GEOCODER']}"
 
-    uri = URI.parse(url)
-    response = Net::HTTP.get_response uri
-    payload = JSON.parse(response.body)
+      uri = URI.parse(url)
+      response = Net::HTTP.get_response uri
+      payload = JSON.parse(response.body)
 
-    @lat = payload["results"][0]["geometry"]["location"]["lat"]
-    @lng = payload["results"][0]["geometry"]["location"]["lng"]
+      @lat = payload["results"][0]["geometry"]["location"]["lat"]
+      @lng = payload["results"][0]["geometry"]["location"]["lng"]
 
-    render 'search'
+      render 'search'
+    end
+  end
+
+  def fileform
+    render 'fileform'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+	root to: 'index#index'
+	
 	get '/' => 'index#index'
+
 	get '/search' => 'index#search'
+
+	post '/file' => 'index#fileform'
 end
